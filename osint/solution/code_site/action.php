d2e711822d8e68e2e023354aebdbe53dc60cd07a
snwox <seonu64@gmail.com>
2020-09-21 20:49:51 +0900

    osint add

---
+++ osint/solution/code_site/action.php
@@ -0,0 +1,16 @@
+<?php
+$temp=$_GET['code']
+if($temp=='everland'){
+  echo "<img src='Ev3rlAnd.png'/>"
+}else if($temp=='samcheok'){
+  echo "<img src='cheok.png'/>"
+}else if($temp=='70000'){
+  echo "<img src='77777.png'/>"
+}else if($temp=='coex_aquarium'){
+  echo "<img src='coex.png'/>"
+}else if($temp=='역삼동'){
+  echo "<img src='pypy.png'/>"
+}else{
+  echo "nono.. incorrect code\n code is lowercase"
+}
+ ?>
